b64dcd451ee9c2b52d5c3ef7c8766c83111925b4
AntonioIGZ <antoniomartinezmunoz@MacBook-Pro-de-Antonio.local>
2016-06-20 20:49:35 +0200

    Initial commit

---
+++ base/basic_item.py
@@ -0,0 +1,14 @@
+class BasicItem:
+    def __init__(self):
+        pass
+
+    def get_methods(self):
+        raise NotImplementedError
+
+    def get_method_info(self):
+        raise NotImplementedError
+
+    def get_properties(self):
+        raise NotImplementedError
+
+
